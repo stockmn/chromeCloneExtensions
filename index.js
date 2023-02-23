4220162a92b66455ff5c7a52aceb92cc7de83d3c
Bonface Maina <mainabonface4@gmail.com>
2023-02-23 22:20:34 +0300

    commi

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 let myLeads =[]
-const inputEl = document.getElementById("input-el").value
-const   inputButton=document.getElementById("input-btn")
+const inputEl = document.getElementById("input-el")
+const inputButton=document.getElementById("input-btn")
 const ulEl= document.getElementById("ul-el")
 
+
+
 inputButton.addEventListener("click",function(){
-    myLeads.push(inputEl)
-    console.log(myLeads)
+    myLeads.push(inputEl.value)
+    inputEl.value=""
+    renderLeads()
+    
 })
-for (let i = 0; i < myLeads.length; i++) {
-    ulEl.innerHTML += "<li> "+myLeads[i]+" </li>"
+function renderLeads(){
+    let listItem=""
+    for (let i = 0; i < myLeads.length; i++) {
+
+        listItem+= `
+        <li>
+           <a href='"+myLeads[i]+"' target='_blank'>"+ myLeads[i]+ "</a>
+        </li>
+        `
+    }
+    ulEl.innerHTML =listItem
  
     
 }
